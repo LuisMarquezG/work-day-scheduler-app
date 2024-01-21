c55c5340bdcb43bb6e11e29761887139a38fd9c9
Luis <luismarquezgarrido@gmail.com>
2024-01-21 16:50:05 +0000

    Creating rows using bootstrap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 //Date dynamic date using day.js
-var today = dayjs()
+var today = dayjs();
 $("#currentDay").text(today.format("dddd, D of MMMM"));
 
 //Day Entries (array of objects as Abdul suggested)
@@ -13,4 +13,25 @@ var workingDay = [
   { time: "03:00 pm", event: "" },
   { time: "04:00 pm", event: "" },
   { time: "05:00 pm", event: "" }
-]
+];
+
+//Rows (using bootstrap)
+workingDay.forEach(function (timeBlock, index) {
+  var timeLabel = timeBlock.time;
+  var blockColor = colorRow(timeLabel);
+  var row =
+    '<div class="time-block" id="' +
+    index +
+    '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+    timeLabel +
+    '</div><textarea class="form-control ' +
+    blockColor +
+    '">' +
+    timeBlock.event +
+    '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"></button></div></div></div>';
+
+  // Adding rows to container div 
+  $(".container").append(row);
+});
+
+
