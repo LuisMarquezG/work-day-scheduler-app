b0f26f722143a76d0e231643d6207d60abf4c177
Luis <luismarquezgarrido@gmail.com>
2024-01-21 15:57:48 +0000

    Creating an array of objects for daily entries

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 //Date dynamic date using day.js
 var today = dayjs()
 $("#currentDay").text(today.format("dddd, D of MMMM"));
+
+//Day Entries (array of objects as Abdul suggested)
+var workingDay = [
+  { time: "09:00 am", event: "" },
+  { time: "10:00 am", event: "" },
+  { time: "11:00 am", event: "" },
+  { time: "12:00 m", event: "" },
+  { time: "01:00 pm", event: "" },
+  { time: "02:00 pm", event: "" },
+  { time: "03:00 pm", event: "" },
+  { time: "04:00 pm", event: "" },
+  { time: "05:00 pm", event: "" }
+]
